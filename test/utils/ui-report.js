5bd0ee3d8cb967cd8d844f2d0444e3667d1619bf
sachin.jadhav222 <sachin.jadhav222@yahoo.com>
2021-05-08 23:21:20 +0100

    ci reports

--- test/utils/ui-report.js
+++ test/utils/ui-report.js
@@ -1,27 +1,26 @@
-const reporter = require('cucumber-html-reporter');
-require('dotenv-flow').config();
+const reporter = require("cucumber-html-reporter");
+require("dotenv-flow").config();
+
 const options = {
-    theme: 'bootstrap',
-    jsonFolder: "reports/ui/",
-    //jsonFile: 'reports/ui/gocity-attractions.json',
-    jsonFile: 'reports/ui/gocity-attractions.json',
-    output: 'reports/ui/ui-test-report.html',
-    screenshotsDirectory: 'reports/ui/',
-    storeScreenshots: true,
-    reportSuiteAsScenarios: true,
-    scenarioTimestamp: true,
-    launchReport: true,
-    name:'UI Automation',
-    brandTitle:'UI-Test Report',
-    metadata: {
-        "Date:": new Date(),
-        "Test Environment": process.env.ENV,
-        "Browser": process.env.BROWSER,
-        "Platform": process.env.PLATFORM,
-        "SDET": process.env.SDET,
-       
-    }
+  theme: "bootstrap",
+  jsonFolder: "./reports/ui/",
+  //jsonFile: 'reports/ui/gocity-attractions.json',
+  jsonFile: "./reports/ui/gocity-attractions.json",
+  output: "./reports/ui/ui-test-report.html",
+  screenshotsDirectory: "./reports/ui/",
+  storeScreenshots: true,
+  reportSuiteAsScenarios: true,
+  scenarioTimestamp: true,
+  launchReport: true,
+  name: "UI Automation",
+  brandTitle: "UI-Test Report",
+  metadata: {
+    "Date:": new Date(),
+    "Test Environment": process.env.ENV,
+    Browser: process.env.BROWSER,
+    Platform: process.env.PLATFORM,
+    SDET: process.env.SDET,
+  },
 };
 
 reporter.generate(options);
-    
